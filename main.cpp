6ddb9c4fbf8f99af20cb06dda4d866974a07182d
Virgile Broillet <115034757+Virgile-Broillet@users.noreply.github.com>
2024-11-20 16:00:37 +0100

    gluck

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 void creeFichierEntiers(const char * nomFichier, int nb)
 //preconditions : nomFichier chaine de caracteres designant le nom du fichier a creer
 //postcondition : le fichier nomFichier contient nb entiers separes par des espaces
-//glucked
 {
   std::ofstream ofs;
   ofs.open(nomFichier);
