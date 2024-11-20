e26b77e51fba9e27da45b62f23dc191f5a6a820b
Virgile Broillet <115034757+Virgile-Broillet@users.noreply.github.com>
2024-11-20 17:05:20 +0100

    update

--- hpp/Image1D.hpp
+++ hpp/Image1D.hpp
@@ -1,1 +1,31 @@
+//
+        /**
+            @author Created by Virgile Broillet P2103804 on 20/11/2024
+            @file Image1D.hpp
+            @brief
+        
+            @copyright Copyright © 2024 - 2024+1 Virgile Broillet P2103804. All rights reserved for studying or personal use.
+        */
+        
 
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Image1D {
+    unsigned int lenght;
+    unsigned int width;
+    
+    vector<int> data;
+    
+public:
+    // Constructeurs
+    Image1D(int lenght, int width);
+    ~Image1D();
+    
+    bool loadPGM(const string& filename); // Charger une image PGM
+    bool savePGM(const string& filename) const; // Sauvegarder en PGM
+
+};
