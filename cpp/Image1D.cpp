9a664d9fa3e5e4e5502a700ea4d2d3721b6cd2f0
Virgile Broillet <115034757+Virgile-Broillet@users.noreply.github.com>
2024-11-20 16:56:49 +0100

    update

--- cpp/Image1D.cpp
+++ cpp/Image1D.cpp
@@ -1,4 +1,24 @@
-#include "../Image1D.hpp"
+//
+        /**
+            @author Created by Virgile Broillet P2103804 on 20/11/2024
+            @file Image1D.cpp
+            @brief
+        
+            @copyright Copyright © 2024 - 2024+1 Virgile Broillet P2103804. All rights reserved for studying or personal use.
+        */
+
+#include "../hpp/Image1D.hpp"
 #include <iostream>
 
-using namespace sdt;
+using namespace std;
+
+Image1D::Image1D(){
+    Image1D * im = new Image1D;
+    im->Largeur = this->Largeur;
+    im->Largeur = this->Largeur;
+    //
+}
+
+Image1D::~Image1D(){
+    
+}
